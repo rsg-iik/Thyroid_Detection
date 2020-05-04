eb13cb9f6e701f4015065fffeecf1cdcddda2045
rsg-iik <55813431+rsg-iik@users.noreply.github.com>
2020-05-04 00:36:51 -0700

    Update DataTransformationPrediction.py

--- DataTransformation_Prediction/DataTransformationPrediction.py
+++ DataTransformation_Prediction/DataTransformationPrediction.py
@@ -29,10 +29,7 @@ class dataTransformPredict:
                                                keep only "Integer" data for ease up the loading.
                                                This column is anyways going to be removed during prediction.
 
-                                   Written By: iNeuron Intelligence
-                                  Version: 1.0
-                                  Revisions: None
-
+                                  
                                           """
 
           try:
